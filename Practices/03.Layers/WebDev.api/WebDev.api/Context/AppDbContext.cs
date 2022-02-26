60a151cc342c0be59e752031ce75b086088fcc41
Roberto-CSV <71284804+Roberto-CSV@users.noreply.github.com>
2022-02-25 20:46:10 -0500

    Initial Version

---
+++ Practices/03.Layers/WebDev.api/WebDev.api/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace WebDev.api.Context
+{
+    using Microsoft.EntityFrameworkCore;
+    using WebDev.api.Model;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}

73e1bc63b09090f6f30f396b12c38425b18e0771
Julio Robles <jucer74@hotmail.com>
2022-02-25 21:34:25 -0500

    Api

--- Practices/03.Layers/WebDev.Api/WebDev.Api/Startup.cs
+++ Practices/03.Layers/WebDev.Api/WebDev.Api/Startup.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using WebDev.Api.Context;
 
 namespace WebDev.Api
 {
@@ -19,12 +17,6 @@ namespace WebDev.Api
     }
 
     public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddRazorPages();
-    }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -49,8 +41,30 @@ namespace WebDev.Api
 
       app.UseEndpoints(endpoints =>
       {
+        endpoints.MapControllerRoute(
+        name: "Users",
+        pattern: "{controller=Users}/{action=Index}/{id?}");
+
         endpoints.MapRazorPages();
       });
+
+      // Enable middleware to serve generated Swagger as a JSON endpoint.
+      app.UseSwagger();
+
+      // Enable middleware to serve swagger-ui (HTML. JS, CSS, etc.),
+      // specifying the Swagger JSON endpoint
+      app.UseSwaggerUI(s => s.SwaggerEndpoint("/swagger/v1/swagger.json", "Users API"));
+
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddRazorPages();
+      services.AddControllers();
+      services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CnnStr")));
+
+      services.AddSwaggerGen(s => s.SwaggerDoc("v1", new OpenApiInfo { Title = "User API", Version = "v1" }));
     }
   }
 }

3b2f0f9c1eaaaa2a4286e73be92d4fa8ff474ebf
xsebasb <61366649+xsebasb@users.noreply.github.com>
2022-03-18 21:36:14 -0500

    ParcialUno

---
+++ Exams/Exam-01/NetBank/NetBank.BusinessLogic/CreditCardBL.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace LuhnAlgorithm.Library
+{
+    public static class LuhnAlgorithmValidator
+    {
+        private static int mod18 = 18;
+        private static int mod10 = 10;
+        public static bool IsValid(string creditCardNumber)
+        {
+            var digitsOnly = GetDigits(creditCardNumber);
+
+
+            if (digitsOnly.Length >= mod18 || digitsOnly.Length < mod10) return false;
+
+            int sum = 0;
+            int digit = 0;
+            int addend = 0;
+            bool timesTwo = false;
+
+            for (var i = digitsOnly.Length - 1; i >= 0; i--)
+            {
+                digit = int.Parse(digitsOnly.ToString(i, 1));
+                if (timesTwo)
+                {
+                    addend = digit * 2;
+                    if (addend > 9)
+                        addend -= 9;
+                }
+                else
+                    addend = digit;
+
+                sum += addend;
+
+                timesTwo = !timesTwo;
+
+            }
+            return (sum % mod10) == 0;
+            return (sum % mod18) == 0;
+
+        }
+        private static StringBuilder GetDigits(string creditCardNumber)
+        {
+            var digitsOnly = new StringBuilder();
+            foreach (var character in creditCardNumber)
+            {
+                if (char.IsDigit(character))
+                    digitsOnly.Append(character);
+            }
+            return digitsOnly;
+        }
+    }
+}
---
+++ Exams/Exam-01/NetBank/NetBank.Models/ReportedCard.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetBank.Models
+{
+    public class ReportedCard
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre de la red emisora es obligatorio")]
+        public string IssuingNetwork { get; set; }
+
+        [Required(ErrorMessage = "El numero de tarjeta es obligatorio")]
+        public string CreditCardNumber { get; set; }
+
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "El estado de la tarjeta es obligatorio")]
+        public string StatusCard { get; set; }
+
+        [Required(ErrorMessage = "La fecha de reporte de la tarjeta es obligatorio")]
+        public string ReportedDate { get; set; }
+
+        [Required(ErrorMessage = "La fecha de actualizacion de la tarjeta es obligatorio")]
+        public string LastUpdatedDate { get; set; }
+    }
+}

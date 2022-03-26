505988dddb88b313ee41a3225b3f3696a2d5922b
Julio Robles <jucer74@hotmail.com>
2022-03-25 21:23:00 -0500

    Update Class

---
+++ Proyecto/ReminderApp/ReminderApp.Domain/Reminder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReminderApp.Domain
+{
+   public class Reminder
+   {
+      public int Ind { get; set; }
+      public int CategoryId { get; set; }
+      public string Description { get; set; }
+      public DateTime StartDate { get; set; }
+      public string CronExpression { get; set; }
+      public int? NumberOfTimes { get; set; }
+      public bool? Enabled { get; set; }
+
+      public virtual Category Category { get; set; }
+   }
+}

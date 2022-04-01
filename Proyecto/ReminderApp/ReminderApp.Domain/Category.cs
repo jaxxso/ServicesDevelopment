5f55067d460803286995f8c0c8d98d1bd56348f9
jcclavijod <47200341+jcclavijod@users.noreply.github.com>
2022-04-01 18:54:17 -0500

    ReminderApp  v1

---
+++ Proyecto/ReminderApp/ReminderApp.Domain/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ReminderApp.Domain
+{
+   public class Category
+   {
+      public Category()
+      {
+         Reminders = new HashSet<Reminder>();
+      }
+
+      public int Id { get; set; }
+      public string Description { get; set; }
+
+      public virtual ICollection<Reminder> Reminders { get; set; }
+   }
+}

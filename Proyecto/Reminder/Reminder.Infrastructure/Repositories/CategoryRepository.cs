ea85628118d4cb9efa90b5d2db5fb57b51622a2a
jcclavijod <47200341+jcclavijod@users.noreply.github.com>
2022-04-01 21:23:50 -0500

    Final Class Actualization

---
+++ Proyecto/Reminder/Reminder.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using Reminder.Domain.Entities;
+using Reminder.Infrastructure.Common;
+using Reminder.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminder.Infrastructure.Repositories
+{
+    public class CategoryRepository : Repository<Category>
+    {
+        public CategoryRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+    
+        public IEnumerable<Reminder.Domain.Entities.Reminder> FindRemindersByCategory(Category category)
+        {
+            return base.Find(c => c.Cate)
+        }
+    }
+}
---
+++ Proyecto/Reminder/Reminder.Infrastructure/Repositories/ReminderRepository.cs
@@ -0,0 +1,19 @@
+﻿using Reminder.Infrastructure.Common;
+using Reminder.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminder.Infrastructure.Repositories
+{
+    public class ReminderRepository : Repository<Reminder.Domain.Entities.Reminder>
+    {
+        public ReminderRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+
+        }
+
+    }
+}

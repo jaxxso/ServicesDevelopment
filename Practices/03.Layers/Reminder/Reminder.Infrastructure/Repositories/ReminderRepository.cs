29c7d78348a607eb9974445692e7f5a2702cf2d7
cris322 <94892991+cris322@users.noreply.github.com>
2022-04-29 12:31:22 -0500

    Parcila2

---
+++ Practices/03.Layers/Reminder/Reminder.Infrastructure/Repositories/ReminderRepository.cs
@@ -0,0 +1,14 @@
+﻿using ReminderAPP.Domain.Entities;
+using ReminderAPP.Domain.Interface.Repositories;
+using ReminderAPP.Infrastructure.Common;
+using ReminderAPP.Infrastructure.Context;
+
+namespace ReminderAPP.Infrastructure.Repositories
+{
+    public class ReminderRepository : Repository<Reminder>, IReminderRepository
+    {
+        public ReminderRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+    }
+}

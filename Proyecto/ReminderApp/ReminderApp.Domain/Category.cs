284b29baa6674046a4a3cf0a6aa14a8d075aeabd
Nabrilg <100003523+Nabrilg@users.noreply.github.com>
2022-04-29 15:24:52 -0500

    w

---
+++ Proyecto/ReminderApp/ReminderApp.Domain/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReminderApp.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<Reminder> Reminders { get; set; }
+    }
+
+}
+
+      

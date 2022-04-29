8490cd2d4bc578a1ae80461cc6a4df5fbe8721f1
Nabrilg <100003523+Nabrilg@users.noreply.github.com>
2022-04-29 15:24:40 -0500

    c1

--- Proyecto/ReminderApp/ReminderApp.Api/Startup.cs
+++ Proyecto/ReminderApp/ReminderApp.Api/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using ReminderApp.Api.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,46 +15,47 @@ using System.Threading.Tasks;
 
 namespace ReminderApp.Api
 {
-   public class Startup
-   {
-      public Startup(IConfiguration configuration)
-      {
-         Configuration = configuration;
-      }
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-      public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-      // This method gets called by the runtime. Use this method to add services to the container.
-      public void ConfigureServices(IServiceCollection services)
-      {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
 
-         services.AddControllers();
-         services.AddSwaggerGen(c =>
-         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Reminder.Api", Version = "v1" });
-         });
-      }
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ReminderApp.Api", Version = "v1" });
+            });
 
-      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-      {
-         if (env.IsDevelopment())
-         {
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Reminder.Api v1"));
-         }
+        }
 
-         app.UseHttpsRedirection();
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ReminderApp.Api v1"));
+            }
 
-         app.UseRouting();
+            app.UseHttpsRedirection();
 
-         app.UseAuthorization();
+            app.UseRouting();
 
-         app.UseEndpoints(endpoints =>
-         {
-            endpoints.MapControllers();
-         });
-      }
-   }
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
 }
---
+++ Proyecto/ReminderApp/ReminderApp.Application/Services/CategoryService.cs
@@ -0,0 +1,57 @@
+﻿using ReminderApp.Application.Interfaces;
+using ReminderApp.Domain.Entities;
+using ReminderApp.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReminderApp.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task AddAsync(Category entity)
+        {
+            await _categoryRepository.AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<Category>> FindAsync(Expression<Func<Category, bool>> predicate)
+        {
+            return await _categoryRepository.FindAsync(predicate);
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _categoryRepository.GetAllAsync();
+        }
+
+        public async Task<Category> GetByIdAsync(int id)
+        {
+            var reminder = await _categoryRepository.GetByIdAsync(id);
+
+            // Validate If Exist
+            return reminder;
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            var reminder = await _categoryRepository.GetByIdAsync(id);
+            await _categoryRepository.RemoveAsync(reminder);
+        }
+
+        public async Task UpdateAsync(int id, Category entity)
+        {
+            // Validate if Exist
+            await _categoryRepository.UpdateAsync(entity);
+        }
+    }
+}

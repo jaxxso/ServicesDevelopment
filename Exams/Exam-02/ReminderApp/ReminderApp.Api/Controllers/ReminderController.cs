c2eacbc4a30cbfc6f053c82e37a5106d6cabfefc
Rosemberg-Diaz <diazmendezdaniel043@gmail.com>
2022-04-29 19:18:58 -0500

    FinalParcial

---
+++ Exams/Exam-02/ReminderApp/ReminderApp.Api/Controllers/ReminderController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReminderApp.Application.Interfaces;
+using ReminderApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ReminderApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReminderController : ControllerBase
+    {
+        private readonly IReminderService _reminderService;
+        private readonly ICategoryService _categoryService;
+        public ReminderController(IReminderService reminderService, ICategoryService categoryService)
+        {
+            _reminderService = reminderService;
+            _categoryService = categoryService;
+        }
+
+        // GET: api/<ReminderController>
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _reminderService.GetAllAsync());
+        }
+
+        // GET api/<ReminderController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await _reminderService.GetByIdAsync(id));
+        }
+
+        // POST api/<ReminderController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Reminder person)
+        {
+            await _reminderService.AddAsync(person);
+            return Ok();
+        }
+
+        // GET api/<ReminderController>/ByCategory/5
+        //[HttpGet("ByCategory/{id}")]
+        //public async Task<IActionResult> Find(Expression<Func<Reminder, bool>> predicate)
+        //{
+          //  return Ok(await _reminderService.FindAsync(predicate));
+        //}
+
+        // GET api/<ReminderController>/ByCategory/5
+        [HttpGet("ByCategoryId/{id}")]
+        public async Task<IActionResult> FindByCategoryId(int id)
+        {
+            return Ok(await _reminderService.FindIdAsync(id));
+        }
+
+        // PUT api/<ReminderController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Reminder person)
+        {
+            await _reminderService.UpdateAsync(id, person);
+            return Ok();
+        }
+
+        // DELETE api/<ReminderController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _reminderService.RemoveAsync(id);
+            return Ok();
+        }
+
+        // DELETE api/<PeopleController>/Category/5
+        [HttpDelete("Category/{id}")]
+        public async Task<IActionResult> DeleteByategoryId(int id)
+        {
+            await _categoryService.RemoveAsync(id);
+            return Ok();
+        }
+    }
+}
---
+++ Exams/Exam-02/ReminderApp/ReminderApp.Infrastructure/Repositories/ReminderRepository.cs
@@ -0,0 +1,25 @@
+﻿using ReminderApp.Domain.Entities;
+using ReminderApp.Domain.Interfaces.Repositories;
+using ReminderApp.Infrastructure.Common;
+using ReminderApp.Infrastructure.Context;
+using System.Collections.Generic;
+
+namespace ReminderApp.Infrastructure.Repositories
+{
+   public class ReminderRepository : Repository<Reminder>, IReminderRepository
+   {
+      public ReminderRepository(AppDbContext appDbContext) : base(appDbContext)
+      {
+      }
+
+      //public IEnumerable<Reminder> FindRemindersByCategory(Category category)
+      //{
+        // return (IEnumerable<Reminder>)base.FindAsync(c => c.Category.Equals(category));
+      //}
+      
+      public IEnumerable<Reminder> FindReminderByCategoryId(int id)
+        {
+            return (IEnumerable<Reminder>)base.FindAsync(c => c.CategoryId.Equals(id));
+        }
+   }
+}

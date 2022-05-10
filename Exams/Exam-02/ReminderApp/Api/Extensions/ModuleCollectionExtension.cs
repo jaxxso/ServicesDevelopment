c1e80deceb4d24f89f4c21fc89ecb1ab97dc9639
Julio Robles <Jucer74@Hotmail.com>
2022-05-10 08:03:27 -0500

    Mover Proyecto a la Ruta correcta

---
+++ Exams/Exam-02/ReminderApp/Api/Extensions/ModuleCollectionExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Application.Interfaces;
+using Application.Services;
+using Domain.Interfaces.Repositories;
+using Infrastructure.Repositories;
+
+namespace People.Api.Extensions
+{
+   public static class ModuleCollectionExtension
+   {
+      public static IServiceCollection AddCoreModules(this IServiceCollection services)
+      {
+         // Services / Use Cases
+         services.AddScoped<ICategoryService, CategoryService>();
+         services.AddScoped<IReminderService, ReminderService>();
+
+            return services;
+      }
+
+      public static IServiceCollection AddInfrastructureModules(this IServiceCollection services)
+      {
+         // Repositories
+         services.AddScoped<ICategoryRepository, CategoryRepository>();
+         services.AddScoped<IReminderRepository, ReminderRepository>();
+
+            return services;
+      }
+   }
+}

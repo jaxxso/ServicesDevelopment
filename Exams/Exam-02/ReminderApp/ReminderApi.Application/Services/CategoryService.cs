d987b6381b2cd51cd4b593eb6da99d830dc5e107
Julio Robles <Jucer74@Hotmail.com>
2022-05-13 19:17:51 -0500

    Update CategoryService.cs

--- Exams/Exam-02/ReminderApp/ReminderApi.Application/Services/CategoryService.cs
+++ Exams/Exam-02/ReminderApp/ReminderApi.Application/Services/CategoryService.cs
@@ -3,54 +3,58 @@ using ReminderApi.Domain.Entities;
 using ReminderApi.Domain.Interfaces.Repositories;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ReminderApi.Application.Services
 {
-    public class CategoryService : ICategoryService
-    {
-        private readonly ICategoryRepository _categoryRepository;
+   public class CategoryService : ICategoryService
+   {
+      private readonly ICategoryRepository _categoryRepository;
+      private readonly IReminderRepository _reminderRepository;
 
-        public CategoryService(ICategoryRepository categoryRepository)
-        {
-            _categoryRepository = categoryRepository;
-        }
+      public CategoryService(ICategoryRepository categoryRepository, IReminderRepository reminderRepository)
+      {
+         _categoryRepository = categoryRepository;
+         _reminderRepository = reminderRepository;
+      }
 
-        public async Task AddAsync(Category entity)
-        {
-            await _categoryRepository.AddAsync(entity);
-        }
+      public async Task AddAsync(Category entity)
+      {
+         await _categoryRepository.AddAsync(entity);
+      }
 
-        public async Task<IEnumerable<Category>> FindAsync(Expression<Func<Category, bool>> predicate)
-        {
-            return await _categoryRepository.FindAsync(predicate);
-        }
+      public async Task<IEnumerable<Category>> FindAsync(Expression<Func<Category, bool>> predicate)
+      {
+         return await _categoryRepository.FindAsync(predicate);
+      }
 
-        public async Task<IEnumerable<Category>> GetAllAsync()
-        {
-            return await _categoryRepository.GetAllAsync();
-        }
+      public async Task<IEnumerable<Category>> GetAllAsync()
+      {
+         return await _categoryRepository.GetAllAsync();
+      }
 
-        public async Task<Category> GetByAsyncId(int id)
-        {
-            var category = await _categoryRepository.GetByAsyncId(id);
-            if (category == null) return null; 
-            return category;
-        }
+      public async Task<Category> GetByAsyncId(int id)
+      {
+         var category = await _categoryRepository.GetByAsyncId(id);
+         if (category == null) return null;
+         return category;
+      }
 
-        public async Task RemoveAsync(int id)
-        {
-            var category = await _categoryRepository.GetByAsyncId(id);
-            if(category != null) await _categoryRepository.RemoveAsync(category);
-            
-        }
+      public async Task RemoveAsync(int id)
+      {
+         var category = await _categoryRepository.GetByAsyncId(id);
+         if (category != null)
+         {
+            await _reminderRepository.RemoveAllByCategoryIdAsync(id);
+            await _categoryRepository.RemoveAsync(category);
+         }
+               
+      }
 
-        public async Task UpdateAsync(int id, Category entity)
-        {
-            await _categoryRepository.UpdateAsync(entity);
-        }
-    }
+      public async Task UpdateAsync(int id, Category entity)
+      {
+         await _categoryRepository.UpdateAsync(entity);
+      }
+   }
 }

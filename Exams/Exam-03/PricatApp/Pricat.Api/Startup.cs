9ea7c3ec23edd1d2a75295c3cc1f6fd425fdb018
Nabrilg <100003523+Nabrilg@users.noreply.github.com>
2022-06-11 12:14:17 -0500

    :)

--- Exams/Exam-03/PricatApp/Pricat.Api/Startup.cs
+++ Exams/Exam-03/PricatApp/Pricat.Api/Startup.cs
@@ -26,23 +26,25 @@ namespace Pricat.Api
 
       public IConfiguration Configuration { get; }
 
-      // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container.
       public void ConfigureServices(IServiceCollection services)
-      {
+        {
+          var connectionString = Configuration.GetConnectionString("CnnStr");
+          services.AddDbContext<AppDbContext>(options => options.UseMySQL(connectionString));
 
-         services.AddControllers();
-         services.AddSwaggerGen(c =>
-         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pricat.Api", Version = "v1" });
-         });
-            var MySqlCnnStr = Configuration.GetConnectionString("CnnStr");
-            services.AddDbContext<AppDbContext>(options => options.UseMySql(MySqlCnnStr, ServerVersion.AutoDetect(MySqlCnnStr)));
-            services.AddCoreModules();
-            services.AddInfrastructureModules();
-        }
+          services.AddControllers();
+          services.AddSwaggerGen(c =>
+          {
+              c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pricat.Api", Version = "v1" });
+          });
 
-      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+          // Add Modules
+          services.AddCoreModules();
+          services.AddInfrastructureModules();
+       }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
       {
          if (env.IsDevelopment())
          {
